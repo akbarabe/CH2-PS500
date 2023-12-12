7d23ae5f3a4663301e3ad827dcadce7b37e663e4
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-12 11:08:00 +0700

    merapihkan struktur file

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 const { initializeApp } = require("firebase/app");
 const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 
